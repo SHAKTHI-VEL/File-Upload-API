76c5c100c89224912c347c1585677d2c8c60169b
SHAKTHI-VEL <shakthivel28203@gmail.com>
2023-09-11 16:59:41 +0530

    file v

--- routes/uploader.js
+++ routes/uploader.js
@@ -14,7 +14,8 @@ router.post('/',upload.single('file'),async(req,res)=>{
 
 router.post('/video',upload.single('file'),async(req,res)=>{
     try {
-        const result=await cloudinary.uploader.upload_large(req.file.path, { resource_type: "video" });
+        const result=await cloudinary.uploader.upload_large(req.file.path, { resource_type: "video" }, 
+        function(error, result) {console.log(result, error)});
         res.status(200).json({"sucess":true,"file_url":result.secure_url});
     } catch (error) {
         res.status(500).json({"sucess":false,"message":"Internal Server Error"})
